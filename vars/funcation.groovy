3af1c99d858afbe58c36be194411803e6082dd2c
deepakprem1234 <70758106+deepakprem1234@users.noreply.github.com>
2021-10-19 18:12:49 +0530

    Update funcation.groovy

--- vars/funcation.groovy
+++ vars/funcation.groovy
@@ -1,4 +1,5 @@
 def call(QA_MAIL_ID){
+                println("In Call Funcation")
                 def subject = "FAILED: Job at STAGE ${env.STAGE_NAME} ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
                 def body = """<br><p><p>Check console output at &QUOT;<a href='${env.BUILD_URL}console'>${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>&QUOT;</p></p><br>""",
                 
