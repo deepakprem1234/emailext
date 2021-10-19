773903ec0326b853884fd51dba80c808a9c63f58
deepakprem1234 <70758106+deepakprem1234@users.noreply.github.com>
2021-10-19 17:54:10 +0530

    Update funcation.groovy

--- vars/funcation.groovy
+++ vars/funcation.groovy
@@ -2,9 +2,9 @@ def emailext ():
             {
                 def subject = "FAILED: Job at STAGE ${env.STAGE_NAME} ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
                 def body = """<br><p><p>Check console output at &QUOT;<a href='${env.BUILD_URL}console'>${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>&QUOT;</p><br>""",
-                to: """${env.QA_MAIL_ID}""", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider']]
-
+                
              emailext (
+                to: """${env.QA_MAIL_ID}""", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider']]         
                 subject: subject,
                 attachLog: true, attachmentsPattern: '*.log',
                 body:body   )
