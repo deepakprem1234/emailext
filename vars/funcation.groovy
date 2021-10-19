67e883507769612d247661638503997f91c096c8
deepakprem1234 <70758106+deepakprem1234@users.noreply.github.com>
2021-10-19 18:20:43 +0530

    Update funcation.groovy

--- vars/funcation.groovy
+++ vars/funcation.groovy
@@ -1,11 +1,11 @@
-def call(QA_MAIL_ID){
+def call(){
                 println("In Call Funcation")
-                def subject = "FAILED: Job at STAGE ${env.STAGE_NAME} ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
-                def body = """<br><p><p>Check console output at &QUOT;<a href='${env.BUILD_URL}console'>${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>&QUOT;</p></p><br>""",
+//                 def subject = "FAILED: Job at STAGE ${env.STAGE_NAME} ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
+//                 def body = """<br><p><p>Check console output at &QUOT;<a href='${env.BUILD_URL}console'>${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>&QUOT;</p></p><br>""",
                 
-             emailext (
-                to: """${QA_MAIL_ID}""", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider']]         
-                subject: subject,
-                attachLog: true, attachmentsPattern: '*.log',
-                body:body   )
+//              emailext (
+//                 to: """${QA_MAIL_ID}""", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider']]         
+//                 subject: subject,
+//                 attachLog: true, attachmentsPattern: '*.log',
+//                 body:body   )
 }
